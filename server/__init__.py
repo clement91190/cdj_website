89abc3d996870cb6eef9df309af46363a2f3b72d
clement91190 <clement@lyft.com>
2016-01-25 19:24:29 -0800

    progress

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app.config['ADMIN_PASSWORD'] = 'secret'
+db = MongoEngine(app)
 
 import urls
--- server/urls.py
+++ server/urls.py
@@ -1,5 +1,6 @@
 from server import app
-from flask import render_template
+from flask import render_template, session, url_for, redirect, request, flash
+import functools
 
 
 @app.route('/test', methods=['POST', 'GET'])
@@ -9,4 +10,36 @@ def test():
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        if password == app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('login.html', next_url=next_url)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('logout.html')
