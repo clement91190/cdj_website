6546806c25f06e53152692451e4f9f6f28b2c4ff
Paul Janin <paul.janin@metz.supelec.fr>
2017-05-12 11:10:04 +0200

    Updated requirements to latest versions

--- instant_server.py
+++ instant_server.py
@@ -1,3 +1,11 @@
+###############################
+#Very basic python app to start the server.
+#Can be used locally for testing.
+#To compile the app, use 'export FLASK_APP=instant_server.py'
+#The app can then be run using 'python -m flask run'
+#It requires to have all the python requirements installed ('pip install requirements.txt')
+##############################
+
 import os
 from server import app
 
@@ -5,5 +13,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = (port == 5000)
     if debug:
+        #A local database is used for testing
         app.config['MONGODB_SETTINGS'] = {'db': 'local_db'}
+        print('Debug mode is enabled. local_db used')
     app.run(host='0.0.0.0', port=port, debug=debug)
